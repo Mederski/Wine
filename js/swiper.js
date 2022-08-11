4ec9f1a4cdbbbed45d58143d754561491dbf1f28
Mederski <miedorn@gmail.com>
2022-08-11 17:31:32 +0600

    after commit

--- js/swiper.js
+++ js/swiper.js
@@ -1,20 +1,37 @@
-const swiper = new Swiper('.swiper', {
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+const events = new Swiper('.events', {
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const offer = new Swiper('.offer', {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.offer-pagination',
+    clickable: true,
+  },
+});
+
+const hero = new Swiper('.hero', {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
